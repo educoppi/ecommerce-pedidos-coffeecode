1094c839678ae97204b4e4cd1f0a132ab30eef42
Beatriz Cardoso <biacardosorocha@gmail.com>
2026-08-18 22:47:19 -0300

    feat: cria constantes

---
+++ Back-end/src/main/java/com/coffeecode/ecommerce/util/PedidoUtils.java
@@ -0,0 +1,6 @@
+private static final double VALOR_POR_QUILO = 7.50;
+private static final double FRETE_MINIMO = 15.00;
+private static final double TAXA_DESCONTO = 0.10;
+private static final double DESCONTO_MAXIMO = 50.00;
+private static final double VALOR_FRETE_GRATIS = 300.00;
+

cb9d78b84f38486374140e6068389e35b6014661
Eduardo Coppi <educoppi8@gmail.com>
2026-08-25 22:31:24 -0300

    feat: cria classe produto

--- Back-end/src/main/java/com/coffeecode/ecommerce/model/Produto.java
+++ Back-end/src/main/java/com/coffeecode/ecommerce/model/Produto.java
@@ -1,1 +1,87 @@
+package com.coffeecode.ecommerce.model;
+
 // Modelo da classe Produto
+public class Produto {
+    private String codigo;
+    private String nome;
+    private String descricao;
+    private double preco;
+    private int quantidadeEmEstoque;
+    private boolean ativo;
+
+    public Produto(){
+        this.ativo = true;
+    }
+
+    public Produto (String codigo, String nome, String descricao, double preco, int quantidadeEmEstoque){
+        this.codigo = codigo;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+        this.ativo = true; // todo produto nasce ativo
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public boolean isAtivo(){
+        return ativo; 
+    }
+
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidadeEmEstoque() {
+        return quantidadeEmEstoque;
+    }
+
+    public void setQuantidadeEmEstoque(int quantidadeEmEstoque) {
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public boolean temEstoqueDisponivel(int quantidadeDesejada){
+        return ativo && this.quantidadeEmEstoque >= quantidadeDesejada;
+    }
+
+    public void baixarEstoque(int quantidade){
+        this.quantidadeEmEstoque = this.quantidadeEmEstoque - quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s - R$ %.2f (%d em estoque)",
+                codigo, nome, preco, quantidadeEmEstoque);
+    }
+}

c22051286c85a272635b4fb451333dc81e847419
EDUARDO BRUNO COPPI <eduardo.bruno@senaisp.edu.br>
2026-09-01 22:40:06 -0300

    feat: cria classe cliente e endereco

--- Back-end/src/main/java/com/coffeecode/ecommerce/model/Cliente.java
+++ Back-end/src/main/java/com/coffeecode/ecommerce/model/Cliente.java
@@ -1,1 +1,62 @@
-// Modelo da classe Cliente
+package com.coffeecode.ecommerce.model;
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private Endereco endereco;
+
+    public Cliente(String nome, String cpf, String email, Endereco endereco){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente [nome=" + nome + ", cpf=" + cpf + ", email=" + email + ", telefone=" + telefone + ", endereco="
+                + endereco.toString() + "]";
+    }
+}
